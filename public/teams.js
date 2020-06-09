6a22b2cd15f5d8bee2e10444e833e59f02ae3911
Adam Bolton <adambolton13@gmail.com>
2020-06-08 21:31:15 -0400

    Teams Page Table after Button Submit
    Sent Ron updated server code

--- public/teams.js
+++ public/teams.js
@@ -13,7 +13,6 @@ const loadDropdown = () => {
     req.open("GET", baseURL + "/loadTeamNames", true);
     req.setRequestHeader('Content-Type', 'application/json');
     req.addEventListener('load', function() {
-        console.log(req)
         if (req.status >= 200 && req.status < 400) {
             payload = JSON.parse(req.responseText);
             makeDropdown(payload);
@@ -27,11 +26,103 @@ const loadDropdown = () => {
 
 const makeDropdown = (rows) => {
     var sel = document.getElementById('nameDropdown');
-    for (const col in rows)
-    {
+    for (const col in rows) {
         var opt = document.createElement('option');
         opt.appendChild(document.createTextNode(rows[col].last_name));
-        opt.value=rows[col].last_name;
+        opt.label = rows[col].last_name;
+        opt.value = rows[col].team_id;
         sel.appendChild(opt);
     }
 }
+
+document.getElementById("nameSubmit").addEventListener("click", () => {
+    var req = new XMLHttpRequest();
+    var dropdown = document.getElementById('nameDropdown');
+
+    var updatePayload = { team_id: null };
+
+    team_id = dropdown.options[dropdown.selectedIndex].value;
+    console.log(team_id);
+    updatePayload.team_id = team_id;
+
+
+    console.log(updatePayload);
+
+    req.open('POST', baseURL + "/loadTeamNames", true);
+    req.setRequestHeader('Content-Type', 'application/json');
+
+    req.addEventListener("load", function() {
+        if (req.status >= 200 && req.status < 400) {
+            payload = JSON.parse(req.responseText);
+            deleteTable();
+            makeTable(payload);
+        } else {
+            var errorMessage = "Error: " + res.statusText;
+            console.log(errorMessage);
+        }
+    });
+    req.send(JSON.stringify(updatePayload));
+    event.preventDefault();
+
+});
+
+const makeTable = (allRows) => {
+    var body = document.getElementsByTagName("body")[0];
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    tbl.className = "table";
+    tbl.id = "ron";
+
+
+    for (var i = 0; i < allRows.length + 1; i++) {
+        if (i == 0) {
+            let headers = Object.keys(allRows[0]);
+            makeHeader(tblBody, headers);
+        } else {
+            makeRow(tblBody, payload[i - 1]);
+        }
+    }
+
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+
+};
+
+const makeHeader = (tblBody, columns) => {
+    var newRow = document.createElement("tr");
+
+    for (let key of columns) {
+        var headerCell = document.createElement("th");
+        var headerCellText = document.createTextNode(key);
+        headerCell.appendChild(headerCellText);
+        newRow.appendChild(headerCell);
+    }
+    tblBody.appendChild(newRow);
+};
+
+
+
+const makeRow = (tblBody, row) => {
+
+    var newRow = document.createElement("tr");
+
+
+    for (const col in row) {
+        var cell = document.createElement("td");
+        var cellText = document.createTextNode(row[col]);
+        cell.appendChild(cellText);
+        newRow.appendChild(cell);
+    }
+    tblBody.appendChild(newRow);
+
+};
+
+const deleteTable = () => {
+    try {
+        tbl = document.getElementById("ron");
+        tbl.remove();
+    } catch {
+        console.log("first table load")
+    }
+};
