dbbc9b256af64f697c44729fde95a208fe7d0ff6
Adam Bolton <adambolton13@gmail.com>
2020-06-09 20:14:45 -0400

    Updated Server with POst Route
    Updated

--- database/server.js
+++ database/server.js
@@ -5,8 +5,9 @@ var cors = require('cors');
 const path = require('path');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.set('port', 5065);
 
 
 app.get("/", function(req, res) {
@@ -26,48 +27,48 @@ app.get("/loadTeams", function(req, res) {
 
 
 //Query to insert into Teams table on Teams Admin page
-app.post('/insertTeam', function(req,res,next){
-    
-    var {first_name, last_name, active_member} = req.body;
+app.post('/insertTeam', function(req, res, next) {
+
+    var { first_name, last_name, active_member } = req.body;
     var q = "INSERT INTO teams (`first_name`, `last_name`, `active_member`) VALUES (?,?,?)";
 
     mysql.pool.query(q, [first_name, last_name, active_member], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 //Query to update Teams table on Teams Admin page
-app.put('/updateTeam',function(req,res,next){
+app.put('/updateTeam', function(req, res, next) {
 
-    var {team_id, first_name, last_name, active_member} = req.body;
+    var { team_id, first_name, last_name, active_member } = req.body;
     var q = "UPDATE teams SET first_name=?, last_name=?, active_member=? WHERE team_id=?";
 
-    mysql.pool.query(q,[first_name, last_name, active_member, team_id], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+    mysql.pool.query(q, [first_name, last_name, active_member, team_id], (err, result) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 //Query to delete from Teams table on Teams Admin page
-app.delete('/deleteTeam',function(req,res,next){
+app.delete('/deleteTeam', function(req, res, next) {
 
     var q = "DELETE FROM teams WHERE team_id=?";
 
     mysql.pool.query(q, req.body.id, (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 //Query to load all seasons rows and send as a JSON string
 app.get("/loadSeasons", function(req, res) {
@@ -81,19 +82,19 @@ app.get("/loadSeasons", function(req, res) {
 });
 
 //Query to insert into seasons table on Seasons Admin page
-app.post('/insertSeason', function(req,res,next){
+app.post('/insertSeason', function(req, res, next) {
 
-    var {year, championship_team_id, runner_up_id} = req.body;
+    var { year, championship_team_id, runner_up_id } = req.body;
     var q = "INSERT INTO seasons (`year`, `championship_team_id`, `runner_up_id`) VALUES (?,?,?)";
 
     mysql.pool.query(q, [year, championship_team_id, runner_up_id], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 
 //Query to load all season teams rows and send as a JSON string
@@ -109,19 +110,19 @@ app.get("/loadSeasonTeams", function(req, res) {
 
 
 //Query to insert into season teams table on Season Teams Admin page
-app.post('/insertSeasonTeam', function(req,res,next){
+app.post('/insertSeasonTeam', function(req, res, next) {
 
-    var {season_id, team_id, made_playoffs, wins, losses, ties, points_scored, points_against} = req.body;
+    var { season_id, team_id, made_playoffs, wins, losses, ties, points_scored, points_against } = req.body;
     var q = "INSERT INTO season_teams (`season_id`, `team_id`, `made_playoffs`, `wins`, `losses`, `ties`, `points_scored`, `points_against`) VALUES (?,?,?,?,?,?,?,?)";
 
     mysql.pool.query(q, [season_id, team_id, made_playoffs, wins, losses, ties, points_scored, points_against], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 
 //Query to load all league dues rows and send as a JSON string
@@ -137,19 +138,19 @@ app.get("/loadDues", function(req, res) {
 
 
 //Query to insert into league_dues table on Dues Admin page
-app.post('/insertDues', function(req,res,next){
+app.post('/insertDues', function(req, res, next) {
 
-    var {season_id, team_id, amount} = req.body;
+    var { season_id, team_id, amount } = req.body;
     var q = "INSERT INTO league_dues (`season_id`, `team_id`, `amount`) VALUES (?,?,?)";
 
     mysql.pool.query(q, [season_id, team_id, amount], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 
 //Query to load all matchups rows and send as a JSON string
@@ -165,28 +166,28 @@ app.get("/loadMatchups", function(req, res) {
 
 
 //Query to insert into matchups table on Matchups Admin page
-app.post('/insertMatchup',function(req,res,next){
+app.post('/insertMatchup', function(req, res, next) {
 
-    var {season_id, week, home_team_id, away_team_id, home_team_score, away_team_score} = req.body;
+    var { season_id, week, home_team_id, away_team_id, home_team_score, away_team_score } = req.body;
     var q = "INSERT INTO matchups (`season_id`, `week`, `home_team_id`, `away_team_id`, `home_team_score`, `away_team_score`) VALUES (?,?,?,?,?,?)";
 
     mysql.pool.query(q, [season_id, week, home_team_id, away_team_id, home_team_score, away_team_score], (err, result) => {
-      if(err){
-        next(err);
-        return;
-      }
-      res.sendStatus(200);
+        if (err) {
+            next(err);
+            return;
+        }
+        res.sendStatus(200);
     });
-  });
+});
 
 
 //Query to select the sums of overall winnings in a table for each team
 app.get("/loadWinnings", function(req, res) {
 
     var q = "select first_name as 'First Name', last_name as 'Last Name', sum(amount) as Winnings from league_dues ld " +
-    "JOIN teams t ON t.team_id = ld.team_id " +
-    "GROUP BY 1,2 " +
-    "ORDER BY Winnings DESC";
+        "JOIN teams t ON t.team_id = ld.team_id " +
+        "GROUP BY 1,2 " +
+        "ORDER BY Winnings DESC";
 
     mysql.pool.query(q, function(err, rows, fields) {
         if (err) throw err;
@@ -206,9 +207,25 @@ app.get("/loadTeamNames", function(req, res) {
     });
 });
 
+app.post("/loadTeamNames", function(req, res, next) {
+    var team_id = req.body.team_id;
+    console.log(team_id);
+    var q = `SELECT year as 'Year', wins as 'Wins', losses as "Losses", ties as "Ties", points_scored as "Points Scored", points_against as "Points Against", made_playoffs as "Made Playoffs" from season_teams st
+    Join seasons s ON s.season_id = st.season_id
+    Where team_id = ?;`;
+
+    mysql.pool.query(q, team_id, (err, result) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.send(JSON.stringify(result));
+    });
+});
+
 //Query to select overall records for selected team on Teams page
 // app.get("/loadRecords", function(req, res) {
-
+var q = `SELECT * FROM matchups WHERE (home_team_id = ? AND away_team_id = ?) OR (home_team_id = ? AND away_team_id = ?);`;
 //     var q = "SELECT * from league_dues";
 
 //     mysql.pool.query(q, function(err, rows, fields) {
@@ -222,11 +239,11 @@ app.get("/loadTeamNames", function(req, res) {
 app.get("/loadStandings", function(req, res) {
 
     var q = "select first_name as 'First Name', last_name as 'Last Name', sum(wins) as Wins, sum(losses) as Losses, sum(ties) as Ties, " +
-    "CONCAT(CAST((sum(wins)*100/sum(wins+losses+ties)) AS CHAR(5)),'%') as WinningPct, " +
-    "sum(made_playoffs) as 'Playoff Appearances' from season_teams st " +
-    "JOIN teams t ON t.team_id = st.team_id " +
-    "GROUP BY 1,2 " + 
-    "ORDER BY Wins DESC, WinningPct DESC";
+        "CONCAT(CAST((sum(wins)*100/sum(wins+losses+ties)) AS CHAR(5)),'%') as WinningPct, " +
+        "sum(made_playoffs) as 'Playoff Appearances' from season_teams st " +
+        "JOIN teams t ON t.team_id = st.team_id " +
+        "GROUP BY 1,2 " +
+        "ORDER BY Wins DESC, WinningPct DESC";
 
     mysql.pool.query(q, function(err, rows, fields) {
         if (err) throw err;
@@ -241,8 +258,6 @@ app.get("/loadStandings", function(req, res) {
 //Query to calculate and display overall record between those teams
 
 
-
-
-app.listen(5065, function() {
-    console.log("App listening on port 5065");
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://flip2.engr/oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
